cc24574f0844dbd5ec069b24cafae150109268a1
JUNLIN1111 <Wu1549983212@163.com>
2025-11-16 23:39:56 +0100

    Refactor data loading and training logic in main_demo.py
    Refactored data loading and training functions to use pickle for loading data and added logging. Updated the run function to handle configuration and model directory management.

--- main_demo.py
+++ main_demo.py
@@ -1,97 +1,91 @@
 """This file provides a demo main scirpt for training with Deep Koopman and computing EWC weights"""
 
-import numpy as np
-import torch
-import torch.optim as optim
-from torch.nn import MSELoss
-from models.deep_koopman import DeepKoopman
 from runner.koopman_runner import KoopmanRunner
-from data.load_pickle_data import load_pickle_data
 import os
+import pickle 
+from config import Config
+from hydra_zen import instantiate, store
+import hydra
+import logging
+import torch
+
+def load_data(mode: str, data_path: str, ratio: float = 0.8):
+    if not os.path.exists(data_path):
+        print(f"[ERROR] File not found: {data_path}")
+        return None, None
+
+    # ============ load data ===============
+    data = pickle.load(open(data_path, 'rb'))
+    data_tensor = torch.from_numpy(data).float()  # (N, 108)
+
+    print(f"[INFO] Loaded data → Tensor: {data_tensor.shape}")
+
+    N = data_tensor.shape[0]
+    if N < 2:
+        return None, None
+
+    state_dim, action_dim = 48, 12
+
+    # ============ create episodes =============
+    episode = [
+        {
+            "cur_state":  data_tensor[t, :state_dim].unsqueeze(0),      # (1,48)
+            "cur_action":     data_tensor[t, state_dim:state_dim+action_dim].unsqueeze(0),  # (1,12)
+            "next_state": data_tensor[t+1, :state_dim].unsqueeze(0),    # (1,48)
+        }
+        for t in range(N - 1)
+    ]
+
+    # =========== Divide train and val =============
+    split = int(len(episode) * ratio)
+    train_ep = episode[:split]
+    val_ep = episode[split:]
+
+    print(f"[INFO] Train steps: {len(train_ep)}, Val steps: {len(val_ep)}")
+    return [train_ep], [val_ep]
 
 
-def load_data(mode, data_path, ratio: int):
-    # data: np.ndarray = np.load(data_path, allow_pickle=True)
-    data: np.ndarray = load_pickle_data(data_path)
-    print(f"[INFO] Data shape: {data.shape}")
-    length = data.shape[0]
-    if mode == "test":
-        train_data = data[1000:1010]
-        val_data = data[9000:9010]
-    elif mode == "train":
-        train_end = int(length * ratio)
-        train_data = data[:train_end]
-        val_data = data[train_end:]
-    else:
-        raise ValueError(f"[ERROR] Unknown mode: {mode}")
-    return train_data, val_data
 
 
-def run(mode="test", data_path=None, model_dir=None, fisher_path=None):
-    """
-    mode:       select between train / test
-    data_path:  path to the npy file.
-                The structure of the data need to be (num_sample, x_t + a_t + x_t1).
-    model_dir:  path to the Deep Koopman model.
-                The model will be save to (or load from) this dir when training (or testing).
-    """
-    assert data_path is not None, "Invalid data path."
-    assert model_dir is not None, "Model path must be specified."
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    print(f"[INFO] Loading Data from {data_path}")
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("main")
+def run(config: Config):
+    stage = config.stage
+    train_data,val_data = load_data(stage,'./koopman_data/plane_20251116_175525.pkl' )
+    runner = KoopmanRunner(config, train_data,val_data)
+    # stage = config.stage
+    model_dir = config.checkpoint_path
+    logger.info(f"{stage=}, {model_dir=}")
+    if stage == "train":
+        # automatically change model dir if exists
+        if model_dir.exists():
+            ids = [int(p.stem) for p in model_dir.parent.iterdir() if p.stem.isdigit()]
+            next_id = max(ids) + 1 if ids else 0
+            model_dir = model_dir.parent / f"{next_id}{model_dir.suffix}"
+        model_dir.mkdir(parents=True)
+        config.checkpoint_path = model_dir
+    runner.run(stage)
 
-    train_data, val_data = load_data(mode, data_path, ratio=0.8)
 
-    # ===== init model and alg ===== #
-    loss_fn = MSELoss()
-    model = DeepKoopman(
-        state_dim=7,
-        action_dim=512 * 2,
-        hidden_sizes=[128] * 3,
-        lifted_dim=128,
-        seed=42,
-    )
-    model.to_device(device)
-    # ewc = EWC(model, data=train_data, loss_fn=loss_fn, device=device)
-    ewc = None
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
-    runner = KoopmanRunner(
-        model=model,
-        ewc_model=ewc,
-        train_data=train_data,
-        val_data=val_data,
-        optimizer=optimizer,
-        loss_fn=loss_fn,
-        device=device,
-        normalize=False,
-        ewc_lambda=100.0,
-    )
+def main():
+    config_name = "class_config"
+    store(Config, name=config_name)
+    store.add_to_hydra_store()
 
-    # ===== start main function ===== #
-    if mode == "train":
-        os.makedirs(os.path.dirname(model_dir) or ".", exist_ok=True)
-        runner.train(
-            max_epochs=100000,
-            model_dir=model_dir,
-            task_id=1,
-            fisher_path=fisher_path,
-            # threshold_mode="neural_ratio",
-            threshold_mode=None,
-            ewc_threshold=1.0,
-        )
-        print(f"[INFO] Model saved to {model_dir}")
-    elif mode == "test":
-        runner.test(model_dir=model_dir)
-    else:
-        raise ValueError(f"[ERROR] Unknown mode: {mode}")
+    config_path = "configs"
+    config_name = "config"
+    config = None
+
+    def convert(cfg):
+        nonlocal config
+        config = instantiate(cfg)
+
+    hydra.main(config_path or None, config_name, None)(convert)()
+    if config:
+        run(Config(**config))
 
 
 if __name__ == "__main__":
-    run(
-        mode="train",
-        data_path="data/open_drawer_20_demos_2025-07-09_21-48-22.pkl",
-        model_dir="logs/test_1.0",
-        # fisher_path="/home/ltx/Koopman4Rob/logs/test/ewc_task1.pt",
-        fisher_path=None,
-    )
+    main()
+
