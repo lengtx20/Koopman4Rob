a2c46c7d6b9f4b62e3a5d811ac4e5f91b4c73e8e
OpenGHz <1352674740@qq.com>
2025-11-29 21:32:50 +0800

    update

--- compare_features.py
+++ compare_features.py
@@ -8,16 +8,20 @@ extractor = Blip2ImageFeatureExtractor(model_path="pretrained_models/blip2-itm-v
 prompt = "The end effector of the robotic arm tries to get close to the QR code attached to the cabinet"
 extractor.load_model()
 
-images = {
-    "batch": "logs/infer/20251129_203911/0/batch",
-    "live": "logs/infer/20251129_203911/0",
-}
-names = ["env_camera.color.image_raw", "follow_camera.color.image_raw"]
-for name in names:
-    features = []
-    for key, dir in images.items():
-        img = cv2.imread(Path(dir) / name / "0.png")[:, :, ::-1].copy()
-        feature = extractor.process_image(img, prompt)["features_proj"].squeeze(0)
-        features.append(feature)
-    print(torch.norm(features[0]) - torch.norm(features[1]).tolist())
-    print(name, torch.norm(features[0] - features[1]).tolist())
+for date in ["20251129_212239"]:
+    for rollout in [0]:
+        images = {
+            "batch": f"logs/infer/{date}/{rollout}/batch",
+            "live": f"logs/infer/{date}/{rollout}",
+        }
+        names = ["env_camera.color.image_raw", "follow_camera.color.image_raw"]
+        for name in names:
+            features = []
+            for key, dir in images.items():
+                img = cv2.imread(Path(dir) / name / "0.png")[:, :, ::-1].copy()
+                feature = extractor.process_image(img, prompt)["features_proj"].squeeze(
+                    0
+                )
+                features.append(feature)
+            print(torch.norm(features[0]) - torch.norm(features[1]).tolist())
+            print(name, torch.norm(features[0] - features[1]).tolist())
--- tests/run.bash
+++ tests/run.bash
@@ -1,5 +1,6 @@
 com="data_loaders.pairwise.gap=10"
 ckpt_id=5
-task_name=data_loaders.task_name=reach_center_new
+task_name="data_loaders.task_name=reach_center_new"
+compare="interactor.extractor.enable=true interactor.model_input_from=env +interactor.save_image=true"
 
-python3 main.py ${com} +infer=act +checkpoint_path=${ckpt_id}/best
+python3 main.py ${com} ${task_name} ${compare} +infer=act +checkpoint_path=${ckpt_id}/best
